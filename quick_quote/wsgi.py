cb72a09e3ad14e601d2ac60fa2455cb1c5cb42c0
Navjot Singh <weavebytes@gmail.com>
2016-08-03 19:21:59 +0530

    changes setttings

--- quick_quote/wsgi.py
+++ quick_quote/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quick_quote.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quick_quote.current_settings")
 
 application = get_wsgi_application()
